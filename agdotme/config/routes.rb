3bfa60e314138b2acb44bf1f2cc72c299155636d
Alex Gould <alexgould17@gmail.com>
2018-03-27 22:15:53 -0700

    Changed routing for HomeController pages

--- agdotme/config/routes.rb
+++ agdotme/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'home/about'
-
-  get 'home/thanks'
+  #HomeController routes Landing (index) is root, Thanks & Bio (about) with top-level url's.
+  root 'home#index'
+  get 'bio' => 'home#about'
+  get 'thanks' => 'home#thanks'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
