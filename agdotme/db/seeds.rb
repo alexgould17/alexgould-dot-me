656d13c2b18aebe89660265302444cba2a7302c2
Alex Gould <alexgould17@gmail.com>
2018-04-12 20:31:16 -0700

    Added Faker gem in gemfile & blog seeding in seeds.db (5 blogs per seed run).

--- agdotme/db/seeds.rb
+++ agdotme/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Blog.create!(
+    title: Faker::Fallout.location,
+    author: Faker::Fallout.character,
+    hook: Faker::Fallout.quote,
+    body: Faker::Fallout.faction
+  )
+end
+
+puts "Seeding finished! The database now has:"
+puts "#{Blog.count} blog posts."
